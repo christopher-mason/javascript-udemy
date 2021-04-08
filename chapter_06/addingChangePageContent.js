586de7bf22acf1f57cb7ae92bc2163a6f5e71ae6
Chris Mason <m1achris43@gmail.com>
2021-04-07 22:33:57 -0400

    Edit Page Content

---
+++ chapter_06/addingChangePageContent.js
@@ -0,0 +1,19 @@
+const para = document.querySelector('p');
+//console.log(para.innerText);
+//para.innerText = 'Ninjas are awesome!'; // you can use += to amend
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
+
+const content = document.querySelector('.content');
+//console.log(content.innerHTML);
+//content.innerHTML = '<h2>this is new h2</h2>';
+
+const people = ['Mario', 'Luigi', 'Chris'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
