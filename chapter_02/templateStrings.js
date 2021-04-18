12204636a49b673b5b27efe188bdcd8064e9990d
Chris Mason <m1achris43@gmail.com>
2021-04-18 17:55:07 -0400

    Played around with Strings

--- chapter_02/templateStrings.js
+++ chapter_02/templateStrings.js
@@ -3,6 +3,20 @@ const title = 'Upside Down';
 const author = 'Chris';
 const likes = 3000;
 
+const pokeset = "Champion's Path";
+const pokemon = "Rainbow Rare Charizard VMAX";
+
+let pokeresult = `The ${pokemon} is in the ${pokeset} set.`;
+
+console.log(pokeresult);
+
+let html = `
+    <h2>${pokeset}</h2>
+    <p>Contains ${pokemon}</p>
+    <span>The ${pokemon} is in the ${pokeset} set.</span>`;
+
+    console.log(html);
+
 // concatenation way
 // let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 // console.log(result);
